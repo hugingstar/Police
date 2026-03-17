49ce97669f486cac0a47d5beca01460a558672f6
503-20\keduit <a3736159@gmail.com>
2026-03-18 08:19:55 +0900

    Load Balancer 실행순서 조정 및 서비스 업데이트

--- web/k8s_web_service.sh
+++ web/k8s_web_service.sh
@@ -1,40 +1,6 @@
 #!/bin/bash
 
-#1. ConfigMap
-echo "Step 1: Deploying config.yaml..."
-if [ -f "config.yaml" ]; then
-    kubectl apply -f config.yaml
-    if [ $? -eq 0 ]; then
-        echo "Successfully applied config.yaml."
-    else
-        echo "Error: Failed to apply config.yaml."
-        exit 1
-    fi
-else
-    echo "Error: config.yaml file not found."
-    exit 1
-fi
-
-echo "------------------------------------"
-
-# 2. web.yaml 실행
-echo "Step 2: Deploying web.yaml..."
-if [ -f "web.yaml" ]; then
-    kubectl apply -f web.yaml
-    if [ $? -eq 0 ]; then
-        echo "Successfully applied web.yaml."
-    else
-        echo "Error: Failed to apply web.yaml."
-        exit 1
-    fi
-else
-    echo "Error: web.yaml file not found."
-    exit 1
-fi
-
-echo "------------------------------------"
-
-# 3. metallb.yaml 실행
+# metallb.yaml 실행
 echo "Step 3: Deploying metallb.yaml..."
 if [ -f "metallb.yaml" ]; then
     kubectl apply -f metallb.yaml
@@ -49,8 +15,32 @@ else
     exit 1
 fi
 
-echo "------------------------------------"
+echo "---------- Metallb 설정 결과 ----------"
+kubectl get ipaddresspool -n metallb-system
+kubectl get l2advertisement -n metallb-system
+echo "--------------------------------------"
+
+# web.yaml 실행
+echo "Step 2: Deploying web.yaml..."
+if [ -f "web.yaml" ]; then
+    kubectl apply -f web.yaml
+    if [ $? -eq 0 ]; then
+        echo "Successfully applied web.yaml."
+    else
+        echo "Error: Failed to apply web.yaml."
+        exit 1
+    fi
+else
+    echo "Error: web.yaml file not found."
+    exit 1
+fi
+
+echo "---------- Web service 설정 결과 ----------"
+kubectl get pod,svc,deploy -n web
+echo "------------------------------------------"
 echo "All deployments are completed successfully."
 
-# 현재 배포된 Pod 및 Service 상태 확인
-kubectl get pod,svc,deploy -n web
+
+
+
+
