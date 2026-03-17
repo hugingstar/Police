09c3aae588bf1cb6dbb363ea8eaf2b9d284e6e32
intaek-10 <bbbx1026@gmail.com>
2026-03-17 12:04:37 +0900

    pv, pvc 삭제 자동화

--- db/volume_auto_down.sh
+++ db/volume_auto_down.sh
@@ -1,40 +1,26 @@
 #!/bin/bash
 
-# YAML 파일들이 위치한 디렉토리 (현재 디렉토리 기준)
-YAML_DIR="/root/Police/db/nfs-pv"
+echo "--- 1. PVC (PersistentVolumeClaim) 삭제 시작 ---"
 
-# 실행할 파일 리스트 (의존성을 고려하여 순서대로 배열)
-# 만약 파일 내부에 PV와 PVC가 같이 있다면 순차 실행하면 됩니다.
-YAML_FILES=(
-    "nfs-pv-web.yaml"
-    "nfs-pv-was.yaml"
-    "nfs-pv-db.yaml"
-    "nfs-pv-monitoring.yaml"
-    "nfs-pv-monitoring-prometheus.yaml"
-)
+# monitoring 네임스페이스
+kubectl delete pvc nfs-pvc-monitoring-prometheus -n monitoring
+kubectl delete pvc nfs-pvc-monitoring-kjs -n monitoring
 
-echo "----------------------------------------------------"
-echo "Kubernetes NFS PV/PVC 리소스 회수(삭제)를 시작합니다."
-echo "----------------------------------------------------"
+# db 네임스페이스
+kubectl delete pvc nfs-pvc-db-ohit -n db
 
-for FILE in "${YAML_FILES[@]}"; do
-    FILE_PATH="$YAML_DIR/$FILE"
+# was 네임스페이스
+kubectl delete pvc nfs-pvc-was-yslee -n was
 
-    if [ -f "$FILE_PATH" ]; then
-        echo "[적용 중] $FILE..."
-        kubectl delete -f "$FILE_PATH"
-        
-        if [ $? -eq 0 ]; then
-            echo "성공: $FILE"
-        else
-            echo "오류: $FILE 적용 실패"
-        fi
-    else
-        echo "경고: $FILE 파일을 찾을 수 없습니다. 건너뜁니다."
-    fi
-    echo "----------------------------------------------------"
-done
+# web 네임스페이스
+kubectl delete pvc nfs-pvc-web-jjh -n web
 
-echo "모든 작업이 완료되었습니다."
-echo "현재 PV/PVC 상태 확인:"
-kubectl get pv,pvc
+echo "--- 2. PV (PersistentVolume) 삭제 시작 ---"
+
+kubectl delete pv nfs-pv-monitoring-prometheus
+kubectl delete pv nfs-pv-monitoring-kjs
+kubectl delete pv nfs-pv-db-ohit
+kubectl delete pv nfs-pv-was-yslee
+kubectl delete pv nfs-pv-web-jjh
+
+echo "--- 모든 삭제 작업이 완료되었습니다. ---"
