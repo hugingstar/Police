12ee1305894cb3b28a3fd9a9e8e33429f6244070
503-20\keduit <a3736159@gmail.com>
2026-03-18 18:51:05 +0900

    웹 서버 변동

--- web/k8s_web_service.sh
+++ web/k8s_web_service.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# metallb.yaml 실행
+# Step 1: metallb.yaml 실행
 echo "Step 1: Deploying metallb.yaml..."
 if [ -f "metallb.yaml" ]; then
     kubectl apply -f metallb.yaml
@@ -20,7 +20,7 @@ kubectl get ipaddresspool -n metallb-system
 kubectl get l2advertisement -n metallb-system
 echo "--------------------------------------"
 
-# web.yaml 실행
+# Step 2: web.yaml 실행
 echo "Step 2: Deploying web.yaml..."
 if [ -f "web.yaml" ]; then
     kubectl apply -f web.yaml
@@ -34,13 +34,5 @@ else
     echo "Error: web.yaml file not found."
     exit 1
 fi
-
-echo "---------- Web service 설정 결과 ----------"
-kubectl get pod,svc,deploy -n web
-echo "------------------------------------------"
-echo "All deployments are completed successfully."
-
-
-
 
 
